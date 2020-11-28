3d953c6076d8a674342e5715d20ea6c17614d6f5
praakhya <67508200+praakhya@users.noreply.github.com>
2020-11-28 13:57:53 +0530

    Manually setting fps = 30

--- maze/environment.py
+++ maze/environment.py
@@ -17,6 +17,7 @@ class Environment():
         self.h = h
         self.fact = fact
         self.ballcol = ballcol
+        self.clock = pygame.time.Clock()
         self.b = Ball(int(0.5*self.fact), int(14.5*self.fact), 0.25*self.fact, self.ballcol,self, 5)
         self.running = True
         self.score=score
@@ -124,7 +125,7 @@ class Environment():
                 else:
                     self.beep() 
             self.draw()
-        #self.clock.tick(60)
+        self.clock.tick(30)
         #pygame.quit()
 
     def handleGems(self,futureball):
